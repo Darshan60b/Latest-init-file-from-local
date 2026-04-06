c0e4d94b8dfbbcfec0eee3e4fade6832a6f61f7c
Darshan 60b <deepakpaliwal55766@gmail.com>
2026-04-06 21:10:37 +0530

    wait

--- cypress/e2e/myfirsttest.cy.js
+++ cypress/e2e/myfirsttest.cy.js
@@ -7,5 +7,6 @@ describe("CSS locators", () => {
     cy.get("#search_query_top").type("T-shirts");
     cy.get("[name='submit_search']").click();
     cy.get(".lighter").contains("T-shirts");
+    cy.wait(5000);
   });
 });
